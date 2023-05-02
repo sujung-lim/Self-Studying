5c1d0c1daa0d41b74e51257083262bf93021c05c
Sujung Lim <99380780+sujung-lim@users.noreply.github.com>
2023-05-02 20:01:01 +0900

    리액트 독학

---
+++ 리액트/state/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="blogApp">
+     <div className='black-nav'>
+      <h4>My Blog</h4>
+     </div>
+     <div className='list'>
+       <h4>맛집 탐방</h4>
+       <p>3월 21일 발행</p>
+     </div>
+     <div className='list'>
+       <h4>맛집 탐방</h4>
+       <p>3월 21일 발행</p>
+     </div>
+     <div className='list'>
+       <h4>맛집 탐방</h4>
+       <p>3월 21일 발행</p>
+     </div>
+    </div>
+  );
+}
+
+export default App;
